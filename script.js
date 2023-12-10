2849cb2362315d4b9df9bededae689f6107e6aa9
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-10 22:46:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	// Regular expression for validating an Email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  // Test the email against the regular expression
+  return emailRegex.test(str);
 }
 
 // Do not change the code below.
